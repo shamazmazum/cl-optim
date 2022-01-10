25197f916fbddfa2a50ce3700707a859c5c9fbd9
Vasily Postnicov <shamaz.mazum@gmail.com>
2022-01-10 08:24:39 +0300

    Randomize tests

--- tests/ci.lisp
+++ tests/ci.lisp
@@ -1,5 +1,6 @@
 (defun do-all()
   (ql:quickload :cl-optim/tests :verbose t)
+  (setq *random-state* (make-random-state t))
   (uiop:quit
    (if (uiop:call-function "cl-optim-tests:run-tests")
        0 1)))
